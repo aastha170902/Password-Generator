49288956bedd6ebe77a896b84c1ee84affc8354a
Priyal Chugh <77205904+priyal6@users.noreply.github.com>
2022-10-02 16:27:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -93,7 +93,7 @@ function getRandomLower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 function getRandomUpper() {
-    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    return 
 }
 function getRandomNumber() {
     return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
